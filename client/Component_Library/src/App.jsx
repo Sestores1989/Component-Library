b2e3ba672045eadbf7a76e42008ac2fa50fed266
Sestores1989 <116611140+Sestores1989@users.noreply.github.com>
2023-07-23 20:44:13 -0500

    Added react-router

--- client/Component_Library/src/App.jsx
+++ client/Component_Library/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1>Component Library</h1>       
+    <Outlet />      
     </>
   )
 }
--- client/Component_Library/src/main.jsx
+++ client/Component_Library/src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from './Screens/HomeScreen.jsx';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+     <Route index={true} path='/' element={<HomeScreen />} />
+     {/* <Route  path='/login' element={<LoginScreen />} />
+     <Route  path='/register' element={<RegisterScreen />} />
+     
+     <Route path='' element={<PrivateRoute />}>
+     <Route  path='/profile' element={<ProfileScreen />} />
+     </Route> */}
+    </Route>
+  )
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
